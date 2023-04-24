4056ce4a3f34f69dcf734667b8e615c284fee093
faustine-van <faustinemuhayemariya44@gmail.com>
2023-04-24 21:09:51 +0000

    updated

--- Vprintf.c
+++ Vprintf.c
@@ -124,6 +124,11 @@ int my_vprintf(const char *format, va_list ap)
 					break;
 				case 'S':
 					S = va_arg(ap, char *);
+					if (S == NULL)
+					{
+						S ="(NULL)";
+						putchar(S[j]);
+					}
 					NonPrintableChar(S);
 					break;
 				default:
