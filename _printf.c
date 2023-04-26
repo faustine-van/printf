3f7129098acd3adf4be29e6a3a7ccbdfc0e05ee9
faustine-van <faustinemuhayemariya44@gmail.com>
2023-04-26 11:04:55 +0000

    updated

--- _printf.c
+++ _printf.c
@@ -4,16 +4,22 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
+/**
+ *_printf - output
+ *@format: format string specifier to be used
+ *Return: len;
+*/
 int _printf(const char *format, ...)
 {
 	va_list ap;
 	int len;
+
 	if (format == NULL)
 	{
-		return(-1);
+		return (-1);
 	}
 	va_start(ap, format);
 	len = my_vprintf(format, ap);
 	va_end(ap);
-	return len;
+	return (len);
 }
